7a129aa1f54f5d09ddb4db92521d5140f516c2ef
Alhanouf Allazzam <115586263+AlhanoufKMLz@users.noreply.github.com>
2026-04-23 17:01:42 +0300

    feat: add task endpoint

--- src/main/java/com/example/tasktracker/Controller/TaskController.java
+++ src/main/java/com/example/tasktracker/Controller/TaskController.java
@@ -2,10 +2,7 @@ package com.example.tasktracker.Controller;
 
 import com.example.tasktracker.ApiResponse.ApiResponse;
 import com.example.tasktracker.Model.Task;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 
@@ -18,6 +15,12 @@ public class TaskController {
     @GetMapping("/get")
     public ArrayList<Task> getTasks(){
         return tasks;
+    }
+
+    @PostMapping("/add")
+    public ApiResponse addTask(@RequestBody Task task){
+        tasks.add(task);
+        return new ApiResponse("Task added successfully");
     }
 
 
